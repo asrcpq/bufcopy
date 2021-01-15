5470b666b9b682b079c020b908968b93420978f0
asrcpq <asrcpq@outlook.com>
2021-01-15 06:41:54 +0000

    remove zsh-autosuggestions so now it is usable

--- bufcopy.zsh
+++ bufcopy.zsh
@@ -6,13 +6,9 @@ bufcopy_proc() {
 	echo -nE "$BUFFER" | xsel -i -b
 }
 bufcopy_fq() {
-	zle history-search-backward
-	eval "$BUFFER" &
+	eval "$BUFFER$POSTDISPLAY" &
 	disown
 	exit
-}
-bufcopy_test() {
-	notify-send "$POSTDISPLAY"
 }
 bindkey '' bufcopy_proc
 zle -N bufcopy_proc
@@ -20,5 +16,3 @@ bindkey '' bufcopy_reset
 zle -N bufcopy_reset
 bindkey '' bufcopy_fq
 zle -N bufcopy_fq
-bindkey '' bufcopy_test
-zle -N bufcopy_test
