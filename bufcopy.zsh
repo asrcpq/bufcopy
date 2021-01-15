3f60463d8a8162c85ffc04511f2774bd1d5d6e59
asrcpq <asrcpq@outlook.com>
2021-01-15 04:36:30 +0000

    add fq

--- bufcopy.zsh
+++ bufcopy.zsh
@@ -5,7 +5,14 @@ bufcopy_reset() {
 bufcopy_proc() {
 	echo -nE "$BUFFER" | xsel -i -b
 }
+bufcopy_fq() {
+	eval "$BUFFER" &
+	disown
+	exit
+}
 bindkey '' bufcopy_proc
 zle -N bufcopy_proc
 bindkey '' bufcopy_reset
 zle -N bufcopy_reset
+bindkey '' bufcopy_fq
+zle -N bufcopy_fq
