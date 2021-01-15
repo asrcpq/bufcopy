0e45742d611ec3001f8fe007c53cec0d7e7aa48a
asrcpq <asrcpq@outlook.com>
2021-01-15 04:50:41 +0000

    fuck autosuggestion, fuck zsh

--- bufcopy.zsh
+++ bufcopy.zsh
@@ -6,9 +6,13 @@ bufcopy_proc() {
 	echo -nE "$BUFFER" | xsel -i -b
 }
 bufcopy_fq() {
+	zle history-search-backward
 	eval "$BUFFER" &
 	disown
 	exit
+}
+bufcopy_test() {
+	notify-send "$POSTDISPLAY"
 }
 bindkey '' bufcopy_proc
 zle -N bufcopy_proc
@@ -16,3 +20,5 @@ bindkey '' bufcopy_reset
 zle -N bufcopy_reset
 bindkey '' bufcopy_fq
 zle -N bufcopy_fq
+bindkey '' bufcopy_test
+zle -N bufcopy_test
